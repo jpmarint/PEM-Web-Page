856a8a370da15553b92c25828b85360a62d7137e
Juan Pablo Marin Trujillo <jpmarint@gmail.com>
2020-06-04 21:21:04 -0500

    install nodemon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 let index = require('../controllers/index')
 
-/* GET home page. */
+/* GET home page. */ 
 router.get('/', index.index);
 
 module.exports = router;
